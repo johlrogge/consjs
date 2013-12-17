160a8747b71b912b27067e2394146138f879e57d
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2013-12-17 22:38:07 +0100

    possible to run single spec

--- spec/cons-spec.js
+++ spec/cons-spec.js
@@ -1,15 +1,37 @@
 (function(){
-  var defmod = function(cons, fn){
-    console.log("hello", cons, fn);
+  
+  var defmod = function(q, cons, fn, _){
+      var run = function(tests){
+          var results = _(tests).pairs().map(function(pair){
+              return q(pair[1]()).then(
+                  function(passed) {
+                      return Q.resolve({name: pair[0], result: passed});
+                  },
+                  function(error){
+                      return Q.reject({name: pair[0], result: error});
+                  });
+          }).valueOf();
+          q.all(results).then(
+              function(rs){
+                  _.each(rs, function(result){
+                      console.log(result.name, " ", result.result);
+                  });
+              });
+      };
+      run({
+          'cons is cons' : function(){
+              var val = cons.cons('head', 'tail');
+              return fn.isCons(val) ? q.resolve(val) : q.reject(val + " is not a cons") },
+      });
   };
 
   if(typeof defined !== 'undefined'){
-      return define(['cons', 'cons/fn'], defmod);
+      return define(['q', 'cons', 'cons/fn', 'lodash'], defmod);
   }
   else if (typeof module !== 'undefined' && module.exports !== 'undefined') {
-      module.exports = defmod(require('cons', 'cons/fn'));
+      module.exports = defmod(require('q'), require('cons'), require('cons/fn'), require('lodash'));
   }
   else {
-      return defmod(cons, cons.fn);
+      return defmod(Q, cons, cons.fn, _);
   }
 })();
